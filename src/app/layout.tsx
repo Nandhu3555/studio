9273cbb5a62eff814d7be5e03bda0291788158db
G NANDHISHWAR REDDY <gnreddy3555@gmail.com>
2025-07-14 12:15:52 +0000

    give a margin to look decent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
                 <BookProvider>
                   <CategoryProvider>
                     <Header />
-                    <main>{children}</main>
+                    <main className="py-4">{children}</main>
                     <Toaster />
                   </CategoryProvider>
                 </BookProvider>
